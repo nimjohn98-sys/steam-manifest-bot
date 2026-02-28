fdf2d6f07cbe4626b3ce05f500550bbd5e381912
nimjohn98-sys <nimjohn98@gmail.com>
2026-02-28 23:38:40 +0100

    Delete scraper_logic.py

--- scraper_logic.py
+++
@@ -1,5 +0,0 @@
-import cloudscraper
-def download_manifest(app_id):
-    s = cloudscraper.create_scraper()
-    r = s.get(f'https://manifest.youngzm.com/api/download/{app_id}')
-    return r.content
